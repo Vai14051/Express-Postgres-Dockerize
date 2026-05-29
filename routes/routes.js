267991386b7188d2cd278a2aefefe60d67bfd4bd
Vai14051 <jhavaibhav13@gmail.com>
2026-05-29 17:37:38 +0530

    2-FD

--- routes/routes.js
+++ routes/routes.js
@@ -1,9 +1,10 @@
 const express  = require("express");
 const router  = express.Router();
 const {createUser} =  require("../controllers/userController");
+const validateUser = require("../middlewares/inputValidator");
 
 // router.get("/user",getAllUsers);
-router.post("/createUser",createUser);
+router.post("/createUser",validateUser,createUser);
 // router.get("/user/:id",getUserById);
 // router.put("/user/:id",updateUser);
 
